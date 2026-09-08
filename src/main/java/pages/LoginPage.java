09768bfcd60482a4e839ab981bbc8fd8399c235c
muhammed yazeen <muhammedyazeentm67@gmail.com>
2026-09-08 22:50:21 +0530

    searchpage,explorepage,searchsteps,searchtest,feat

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -5,4 +5,5 @@ package pages;
  * Author: Jothi Sri
  */
 public class LoginPage extends BasePage {
+
 }
--- src/test/java/runners/TestSuiteRunner.java
+++ src/test/java/runners/TestSuiteRunner.java
@@ -1,8 +1,15 @@
 package runners;
 
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
 /**
  * TestSuiteRunner — JUnit Platform Suite runner for Cucumber features.
  * Author: Nitheesh
  */
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
 public class TestSuiteRunner {
 }
